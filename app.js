252908a6c631198f165a436076181f09960df60e
abdalrahman20000 <abd.m.ata.2000@gmail.com>
2024-06-04 16:06:21 +0300

    update javascript file add validation to gender field

--- app.js
+++ app.js
@@ -6,20 +6,36 @@ if (user_name == null || user_name == undefined) {
 
 let user_geneder = prompt("Enter Your Gender :");
 
+while (user_geneder != "male" && user_geneder != "female") {
+    alert("Wrong Entery Try againe");
+    user_geneder = prompt("Enter Your Gender : male or female");
+}
+
 if (user_geneder == "male") {
     alert("Welcome Mr " + user_name);
 }
 else if (user_geneder == "female") {
     alert("Welcome Ms " + user_name);
 }
-else {
-    alert("Welcome " + user_name);
-}
 
-let user_order = prompt("Enter your order 1-Dount 2-Coffee 3-Ice cream 4-Bakery");
+let order_type = prompt("what do you want 1-Dount 2-Coffee 3-Ice cream 4-Bakery");
+
+
+let user_order = prompt("Enter your order : ");
 
 user_order.toUpperCase();
 
 alert("Your order is getting prepared");
 
-console.log(user_order);
+// console.log(user_order);
+
+
+let user_info = [user_name, user_geneder, order_type, user_order];
+
+console.log("user information");
+
+for (let i = 0; i <= user_info.length; i++) {
+    console.log(user_info[i]);
+}
+
+
